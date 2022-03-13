ced1e8b883e25020ef13fc7daf1d2955a16a9b94
Dmitry Borodachev <borodachev.58@mail.ru>
2022-03-13 11:57:36 +0300

    исправил баг из-за которого не была видна библиотека

--- dto/src/main/java/liga/medical/DeviceIdentificationDto.java
+++ dto/src/main/java/liga/medical/DeviceIdentificationDto.java
@@ -5,11 +5,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class DeviceIdentificationDto {
+public class DeviceIdentificationDto implements Serializable {
 
     private Long deviceId;
 
--- dto/src/main/java/liga/medical/MedicalData.java
+++ dto/src/main/java/liga/medical/MedicalData.java
@@ -2,11 +2,13 @@ package liga.medical;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * Медицинские данные.
  */
 @Data
-public class MedicalData {
+public class MedicalData implements Serializable {
 
     /**
      * Температура.
